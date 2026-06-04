58adb9c48c643aa9b7532956379390745d34910c
dinesh <dineshmanikanta016@gmail.com>
2026-06-04 10:11:49 +0530

    local testing going well

---
+++ App.js
@@ -0,0 +1,81 @@
+const {Pool} = require("pg")
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
+const app = express()
+
+app.use(express.json())
+
+const pool = new Pool({connectionString: 'postgres://019e8dd0-7535-786e-baaf-123851f830f3:cc878fd6-f81f-4b4e-8097-41a57ee17504@us-west-2.db.thenile.dev:5432/british_auction'})
+
+let client = null
+
+const initializeDbAndServer = async () => {
+    try {
+        client = await pool.connect();
+        app.listen(3001, () => {
+            console.log(`app started at localhost: 3001`)
+        })
+    }
+    catch(error) {
+        console.log(`DB Error : ${error.message}`)
+        process.exit(1)
+    }
+}
+
+initializeDbAndServer()
+
+// API 1 GET (to get the list of all users)
+app.get('/users', async (request, response) => {
+    try {
+        const dbResponse = await client.query(`SELECT * FROM users;`)
+        const users = dbResponse.rows
+        response.json(users)
+    }
+    catch(e) {
+        console.log(`Failed to get the All users: ${e.message}`)
+        process.exit(1)
+    }
+})
+
+// API 2 GET (to get the specific user from the db)
+app.get('/users/:userId', async (request, response) => {
+    try {
+        const {userId} = request.params
+        const dbResponse = await client.query(`SELECT * FROM users WHERE user_id='${userId}';`)
+        response.json(dbResponse.rows)
+    }
+    catch(e) {
+        console.log(`Failed to get the user: ${e.message}`)
+        process.exit(1)
+    }
+})
+
+// API 3 (to Login the user)
+app.post('/user/login', async (request, response) =>  {
+    try {
+        const {email, password} = request.body
+        const dbResponse = await client.query(`SELECT *
+                                             FROM users
+                                             WHERE email = '${email}';`)
+        const data = dbResponse.rows
+        if (data.length === 0) {
+            response.json({ok: false, message: 'Email Not Found'})
+        }
+        else {
+            const originalHashedPassword = data[0].password
+            const isPasswordCorrect = await bcrypt.compare(password, originalHashedPassword)
+            if (isPasswordCorrect) {
+                const jwt_token = jwt.sign({email, password}, 'dinesh')
+                response.json({ok: true, jwt_token})
+            }
+            else {
+                response.json({ok: false, message: 'Invalid Password'})
+            }
+        }
+    } catch (e) {
+        console.log(`Login Error : ${e.message}`)
+        process.exit(1)
+    }
+})
